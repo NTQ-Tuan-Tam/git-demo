66dc82a4c6e16cbf92e127775aa6e655cd847013
tuantamNTQ <tuan.vu2@ntq-solution.com.vn>
2022-11-15 10:12:30 +0700

    consoleApp2-name

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            Console.WriteLine(" tooi ten la :" + "" + Name);
+            Console.ReadKey();
+        }
+    }
+}
